38342636a0b47d4a3f6ac9701f8c5e3c45324e11
aonemd <aonemdsaleh@gmail.com>
2018-11-20 20:20:43 +0200

    Setup OpenGL

--- sky_hopper.c
+++ sky_hopper.c
@@ -1,3 +1,66 @@
-int main() {
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "GL/freeglut.h"
+#include "GL/gl.h"
+
+void render() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glFlush();
+}
+
+void update() {
+	glutPostRedisplay();
+}
+
+void handle_timer(int val) {
+	(void) val;
+
+	glutTimerFunc(5000, handle_timer, 0);
+}
+
+void handle_keyboard(unsigned char key, int mouse_x, int mouse_y) {
+	(void) mouse_x;
+	(void) mouse_y;
+
+	switch(key) {
+		case 27:
+		case 'q':
+			exit(0);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutInitWindowSize(1000, 1000);
+	glutInitWindowPosition(150, 150);
+	glutCreateWindow("Sky Hopper");
+
+	glutFullScreen();
+	glutDisplayFunc(render);
+	glutIdleFunc(update);
+	glutTimerFunc(0, handle_timer, 0);
+	glutKeyboardFunc(handle_keyboard);
+
+	glEnable(GL_DEPTH_TEST);
+
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+    gluPerspective(20.0f, 1000.0f/1000.0f, 0.1f, 300.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 4.0, 15.0, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	glutMainLoop();
+
 	return 0;
 }
