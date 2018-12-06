c85b134742085423f7c0566c1e17835fbad00626
aonemd <aonemdsaleh@gmail.com>
2018-12-06 15:39:20 +0200

    Add a condition on character's y for coin collision

--- coinage.c
+++ coinage.c
@@ -26,7 +26,8 @@ void Coinage__update(Coinage *self) {
 bool Coinage__hide_intersecting_coins(Coinage *self, Character *character) {
 	for (int i = 0; i < NUMBER_OF_COINS; i++) {
 		if (fabs(self->coins[i]->z - character->z) <= 0.07 &&
-				(fabs(self->coins[i]->x - character->x) <= 0.068097)) {
+				(fabs(self->coins[i]->x - character->x) <= 0.068097) &&
+				character->y <= 1) {
 			self->coins[i]->visible = false;
 			return true;
 		}
